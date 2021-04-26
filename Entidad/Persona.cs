a40ddfa3cc8c8fa96ca3ddf1cc0e09e1372f6bed
camilo andres britto quintero <elpepo150@hotmail.com>
2021-04-26 16:56:03 -0500

    Agregar archivos de proyecto.

---
+++ Entidad/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+   public  class Persona
+    {
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+    }
+}
---
+++ Entidad/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    class Producto
+    {
+        public string CodigoProducto { get; set; }
+        public string NombreProducto { get; set; }
+        public decimal PrecioUnidad { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Descuento { get; set; }
+
+
+    }
+}
